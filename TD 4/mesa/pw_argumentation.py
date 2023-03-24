e92566ecd7848e040d869853f6002c623a3823b5
moustapha LO <moustapha.lo@student-cs.fr>
2023-03-24 11:43:13 +0100

    generate pref

--- TD 4/mesa/pw_argumentation.py
+++ TD 4/mesa/pw_argumentation.py
@@ -3,6 +3,11 @@ from mesa.time import RandomActivation
 
 from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
+from communication.preferences.Preferences import Preferences
+from communication.preferences.CriterionName import CriterionName
+from communication.preferences.CriterionValue import CriterionValue
+from communication.preferences.Value import Value
+from communication.preferences.Item import Item
 
 
 class ArgumentAgent( CommunicatingAgent ) :
@@ -19,9 +24,21 @@ class ArgumentAgent( CommunicatingAgent ) :
         return self.preference
 
     def generate_preferences( self , List_items ):
-        # see question 3
-        # To be completed
-        pass
+        self.preference.set_criterion_name_list([CriterionName.PRODUCTION_COST, CriterionName.ENVIRONMENT_IMPACT,
+                                        CriterionName.CONSUMPTION, CriterionName.DURABILITY,
+                                        CriterionName.NOISE])
+
+        for item in List_items:
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.PRODUCTION_COST,
+                                                        Value.VERY_GOOD))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.CONSUMPTION,
+                                                        Value.GOOD))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.DURABILITY,
+                                                        Value.VERY_GOOD))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.ENVIRONMENT_IMPACT,
+                                                        Value.VERY_BAD))
+            self.preference.add_criterion_value(CriterionValue(item, CriterionName.NOISE,
+                                                        Value.VERY_BAD))
 
 class ArgumentModel( Model ) :
     """ ArgumentModel which inherit from Model .
@@ -30,7 +47,8 @@ class ArgumentModel( Model ) :
         self.schedule = RandomActivation( self )
         self.__messages_service = MessageService( self.schedule )
         for i in range(2):
-            a = ArgumentAgent(i, self, "Agent" + str(i), [])
+            agent_pref = Preferences()
+            a = ArgumentAgent(i, self, "Agent" + str(i), agent_pref )
             self.schedule.add(a)
             a.generate_preferences()
             self.schedule.add(a)
