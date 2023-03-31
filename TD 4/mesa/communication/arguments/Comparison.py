0599210461333713f2a1e950aea9c913b9e7f611
moustapha LO <moustapha.lo@student-cs.fr>
2023-03-31 11:03:23 +0200

    c

--- TD 4/mesa/communication/arguments/Comparison.py
+++ TD 4/mesa/communication/arguments/Comparison.py
@@ -1,3 +1,4 @@
+
 class Comparison:
     """Comparison class.
 This class implements a comparison object used in argument object.
@@ -7,5 +8,8 @@ attr: best_criterion_name: worst_criterion_name:
     def __init__(self,best_criterion_name, worst_criterion_name) -> None:
         self.best_criterion_name = best_criterion_name
         self.worst_criterion_name = worst_criterion_name
+
+    def __str__(self):
+        return str(self.best_criterion_name) + " > " + str(self.worst_criterion_name)
 
     
