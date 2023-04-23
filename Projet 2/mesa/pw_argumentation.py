ae223831c78ea5b6c4f3499c3c56e28a0fd20d2a
moustapha LO <moustapha.lo@student-cs.fr>
2023-04-23 21:46:47 +0200

    readme

--- Projet 2/mesa/pw_argumentation.py
+++ Projet 2/mesa/pw_argumentation.py
@@ -14,6 +14,7 @@ from communication.preferences.Value import Value
 from communication.preferences.Item import Item
 from communication.arguments.Argument import Argument
 from communication.arguments.Comparison import Comparison
+from communication.ArgumentAgent import ArgumentAgent
 
 
 with open('data.json') as f:
@@ -50,228 +51,6 @@ def transform_name(value):
         return CriterionName.NOISE
     
 
-class ArgumentAgent( CommunicatingAgent ) :
-    """ ArgumentAgent which inherit from CommunicatingAgent .
-    """
-    def __init__( self , unique_id , model , name , preferences ) :
-        super().__init__( unique_id , model , name )
-        self.preference = preferences
-        self.comparison = []
-        self.supporting_proposals = {}
-        for item in List_items:
-            #print(List_items)
-            self.supporting_proposals[item] = []
-        
-
-
-    def step( self ) :
-        super().step()
-        list_messages = self.get_new_messages()
-        for message in list_messages:
-            send = message.get_exp()
-            dest = message.get_dest()
-            dest_a = service.find_agent_from_name(dest)
-            item = message.get_content()
-            # PROPOSE
-            if message.get_performative() == MessagePerformative.PROPOSE:
-                if dest_a.get_preference().is_item_among_top_10_percent(item, List_items):
-                    self.send_message(Message(dest, send, MessagePerformative.ACCEPT, item))
-                #elif not dest_a.get_preference().is_item_among_top_10_percent(item, List_items):
-                #    pref_item = self.preference.most_preferred(List_items)
-                #    self.send_message(Message(dest, send, MessagePerformative.PROPOSE, pref_item))
-                else:
-                    self.send_message(Message(dest, send, MessagePerformative.ASK_WHY, item))
-            # COMMIT
-            elif message.get_performative() == MessagePerformative.COMMIT:
-                self.model.running = False
-                self.send_message(Message(dest, send, MessagePerformative.COMMIT, item))    
-                
-            # ACCEPT
-            elif message.get_performative() == MessagePerformative.ACCEPT:
-                self.send_message(Message(dest, send, MessagePerformative.COMMIT, item))
-
-            # ASK_WHY
-            elif message.get_performative() == MessagePerformative.ASK_WHY:
-                argument = Argument(boolean_decision=True, item=item)
-                proposal = self.support_proposal(item)
-                argument.add_premiss_couple_values(proposal.get_criterion_name(), proposal.get_value())
-                self.send_message(Message(dest, send, MessagePerformative.ARGUE, argument))
-            
-            # ARGUE
-            elif message.get_performative() == MessagePerformative.ARGUE:
-                #print("Hey")
-                argument = message.get_content()
-                #print(argument)
-                item = argument.item
-                attack = self.attack_argument(argument)
-                #print(attack)
-                #print("Comparaison : ", self.comparison , "\n")
-                if attack[0]:
-                    
-                    #print("I counter-argue")
-                    counter_argument = Argument(boolean_decision=False, item=item)
-                    if attack[1] in (1,2):
-                        crit = [c for c in self.preference.get_criterion_value_list() if c.get_item() == item][0]
-                        comparison = (crit.get_criterion_name(), argument.couple_values.criterion_name)
-                        if comparison not in self.comparison:
-                            self.comparison.append(comparison)
-                            counter_argument.add_premiss_comparison(crit.get_criterion_name(),argument.couple_values.criterion_name)
-                            counter_argument.add_premiss_couple_values(crit.get_criterion_name(),crit.get_value() )
-                            self.send_message(Message(dest, send, MessagePerformative.ARGUE,counter_argument ))
-                            break
-                        #self.send_message(Message(dest, send, MessagePerformative.ACCEPT, item))
-                    if attack[1] == 3:
-                        self.send_message(Message(dest, send, MessagePerformative.PROPOSE, self.preference.most_preferred(List_items)))
-                        break
-                    if attack[1] == 4:
-                        counter_argument = Argument(boolean_decision=True, item=item)
-                        crit = [c for c in self.preference.get_criterion_value_list() if c.get_item() == item][1]
-                        comparison = (crit.get_criterion_name(), argument.couple_values.criterion_name)
-                        
-                        if comparison not in self.comparison:
-                            self.comparison.append(comparison)
-                            counter_argument.add_premiss_comparison(crit.get_criterion_name(),argument.couple_values.criterion_name)
-                            counter_argument.add_premiss_couple_values(crit.get_criterion_name(),crit.get_value() )
-                            self.send_message(Message(dest, send, MessagePerformative.ARGUE,counter_argument ))
-                            break
-                        
-                    
-                if self.support_proposal(item) is not None:
-                    argument = Argument(boolean_decision=True, item=item)
-                    proposal = self.support_proposal(item)
-                    argument.add_premiss_couple_values(proposal.get_criterion_name(), proposal.get_value())
-                    self.send_message(Message(dest, send, MessagePerformative.ARGUE, argument))
-                    break
-
-
-                
-                self.send_message(Message(dest, send, MessagePerformative.ACCEPT, item))
-                    
-                
-
-    def get_preference( self ):
-        return self.preference
-    
-    
-    def generate_preferences( self , List_items ):
-        agent_data = data[str(self.unique_id)]
-        # get the list of criteria, non-ordered
-        criteria_list = agent_data["Criteria_List"].keys()
-        # sort criteria_list according to the values of the dictionary
-        criteria_list = sorted(criteria_list, key=lambda x: agent_data["Criteria_List"][x])
-        # transform the criteria_list into a list of CriterionName
-        criteria_list = [transform_name(x) for x in criteria_list]
-        self.preference.set_criterion_name_list(criteria_list)
-        for item in List_items:
-            item_data = agent_data[item.get_name()]
-            for criterion in self.preference.get_criterion_name_list():
-                criterion_value = transform_value(item_data[criterion.name])
-                self.preference.add_criterion_value(CriterionValue(item, criterion, criterion_value))
-    
-
-    def List_Supporting_Proposal(self, item):
-        """ Generate a list of premisses which can be used to support an item
-        param item : Item - name of the item
-        return : list of all premisses PRO an item ( sorted by order of importance
-        based on agents preferences )
-        """
-        # To be completed
-        
-        for criterion in self.preference.get_criterion_value_list():
-            if criterion.get_item() == item:
-                criterion_value = self.preference.get_value(item, criterion.get_criterion_name())
-                if criterion_value in [Value.GOOD, Value.VERY_GOOD]:
-                    self.supporting_proposals[item].append(criterion)
-                    #print("Supporting proposal : ", self.supporting_proposals[item])
-        
-        
-    
-
-    def List_Attacking_Proposal(self, item):
-        """ Generate a list of premisses which can be used to attack an item
-        param item : Item - name of the item
-        return : list of all premisses CON an item ( sorted by order of importance
-        based on agents preferences )
-        """
-        # To be completed
-        attacking_proposals = []
-
-        for criterion in self.preference.get_criterion_value_list():
-            if criterion.get_item() == item:
-                criterion_value = self.preference.get_value(item, criterion.get_criterion_name())
-                if criterion_value in [Value.BAD, Value.VERY_BAD]:
-                    attacking_proposals.append(criterion)
-
-        return attacking_proposals
-
-
-    def support_proposal(self , item ) :
-        """
-        Used when the agent receives " ASK_WHY " after having proposed an item
-        param item : str - name of the item which was proposed
-        return : string - the strongest supportive argument
-        """
-        # To be completed
-        if len(self.supporting_proposals[item]) == 0:
-            return None
-        proposal = self.supporting_proposals[item][0]
-        self.supporting_proposals[item].remove(proposal)
-        return proposal
-
-
-    def parse_argument(self, argument):
-        """ Parse an argument and return the list of premisses
-        param argument : Argument
-        return : list of CriterionValue
-        """
-        # To be completed
-        return [argument.comparison , argument.couple_values]
-
-
-    def attack_argument(self , argument ) :
-        """
-        Write a method that decides whether an argument can be attacked or not. In our context the
-        agent can attack or contradict an argument provided by another agent if:
-        • The criterion is not important for him (regarding his order)
-        • Its local value for the item is lower than the one of the other agent on the considered criteria
-        • He prefers another item and he can defend it by an argument with a better value on the
-        same criterion.
-        """
-        # The criterion is not important for him (regarding his order)
-        if argument.couple_values is not None:
-            criterion = argument.couple_values.criterion_name
-            value = argument.couple_values.value
-            if criterion in self.preference.get_criterion_name_list()[-1:-3:-1]:
-                return True, 1
-
-            # Its local value for the item is lower than the one of the other agent on the considered criteria
-            b = False
-            for crit in self.preference.get_criterion_value_list():
-                if crit.get_criterion_name() == criterion and crit.get_value() in [Value.BAD, Value.VERY_BAD]:
-                    b = True
-                    cr = crit
-                    break
-            if b and value in [Value.GOOD, Value.VERY_GOOD]:
-                return True, 2
-        
-        if argument.comparison is not None:
-            b_criterion = argument.comparison.best_criterion_name
-            w_criteria = argument.comparison.worst_criterion_name
-            for crit in self.preference.get_criterion_name_list()[:2]:
-                if crit != b_criterion:
-                    return True, 4
-                
-
-
-
-        # He prefers another item and he can defend it by an argument with a better value on the same criterion.
-        criterion = argument.couple_values.criterion_name
-        preferred_item = self.preference.most_preferred(List_items)
-        if criterion is not None and preferred_item != argument.item :
-            return True, 3
-
-        return False, 0
-    
 
 class ArgumentModel( Model ) :
     """ ArgumentModel which inherit from Model .
@@ -282,7 +61,7 @@ class ArgumentModel( Model ) :
         self.max_steps = max_steps
         for i in range(N):
             agent_pref = Preferences()
-            a = ArgumentAgent(i, self, "Agent" + str(i), agent_pref )
+            a = ArgumentAgent(i, self, "Agent" + str(i), agent_pref , List_items)
             a.generate_preferences(List_items)
             a.List_Supporting_Proposal(List_items[0])
             a.List_Supporting_Proposal(List_items[1])
@@ -306,7 +85,10 @@ if __name__ == '__main__':
     
     sender = agents[0].get_name()
     receiver = agents[1].get_name()
-    message = Message(sender, receiver, MessagePerformative.PROPOSE, List_items[0])
+    first_item = agents[0].preference.most_preferred(List_items)
+    message = Message(sender, receiver, MessagePerformative.PROPOSE, first_item)
+    agents[0].item_proposed.append(first_item)
+    agents[0].proposed_item = first_item
 
     agents[0].send_message(message)
   
--- README.md
+++ README.md
@@ -3,39 +3,36 @@
 
 ## Projet 2 : Argumentation-based dialogue for choosing a sport
 ### Overview
-The goal is to simulate an argumentation-based dialogue between artificial agents which will interact to make a joint decision regarding choosing the best sport. One can then imagine several applications. For instance by studying the most impactful arguments.
+The goal of this simulation is to conduct an argumentation-based dialogue between 10 artificial agents, each representing a different preference for criteria and items related to sports. These agents will interact with each other in a structured dialogue format to make a joint decision regarding the best sport. 
 
 ### Agent Preferences
 #### Data about sports
-We prepare a data.json file beforehand. An object in this file represents an agent (the implementation is meant for 2 so far). It is represented by an integer and comes with a "Criteria_List" property which gives the ordered criteria importance for the agent. Then there is a property for each of the sports to compare. Each property contains a list of key-value pairs to capture the agent's rating of the sport for each criteria. The file Value.py defines the possible ratings and CriteriaName.py defines the criteria list we chose:
-* a
-* b
-* c
+The preferences for each agent will be randomly generated using a "get_data" script, which will assign random values to criteria such as popularity, physical_intensity, skill_required, team_play and entertainment_value. The agents will engage in arguments and counterarguments, presenting their preferences and supporting them with arguments based on their preferences. The dialogue will be structured using a predetermined set of rules and guidelines for argumentation. The generated json file contains for agent ratings for each criterion on a scale of 1 to 5, and has also ratinns for each sport for these criteria using values such as "VERY_GOOD", "GOOD", "BAD", or "VERY_BAD". 
 #### Get preferences
 How an agent sets and compares their preferences regarding different sports and criteria is defined in the Preference class which will be an attribute of an agent object. The method generate_preferences of the ArgumentAgent class enables to generate the preferences object by reading the data file. There are 2 auxiliary functions transform_name and transform_value to switch from strings in the json file to correct criteria names and values.
 
 ### Communication
 We use 5 message performatives: PROPOSE - ASK_WHY - ARGUE - ACCEPT - COMMIT.
-The ArgumentModel class initializes a simulation environment with a number of agents, each with their own set of preferences generated using the Preferences class. The number of agents and maximum number of simulation steps are passed as arguments. A RandomActivation object is created to schedule the agents, and a MessageService object is created to handle message passing between the agents.
+The ArgumentModel class initializes a simulation environment with a number of agents ( 2 in oour study randomly), each with their own set of preferences generated using the Preferences class. The number of agents and maximum number of simulation steps are passed as arguments. A RandomActivation object is created to schedule the agents, and a MessageService object is created to handle message passing between the agents. We have modify the provieded code to initialize a MessageService object at the beginning with a basic scheduler which will help us use this object for each simulation of dialogue.
 
 ### Arguments
-An argument can consist of either a couple-value, for example: "Soccer <= CriterionName.ENTERTAINMENT = Value.VERY_GOOD"
-The method parse_argument, takes an argument and returns the list of premises. It simply returns a list consisting of the comparison and the couple-values of the given argument.
+An argument can consist of a couple-value, for example: "Soccer <= CriterionName.ENTERTAINMENT = Value.VERY_GOOD" or a comparison between two criteria or both.
 
 ### Selecting arguments
 #### Support
 The method List_Supporting_Proposal, takes a sport as a parameter and generates a list of premises that can be used to support the sport. It iterates through the preference criteria of the agent and checks if the criterion is related to the given sport. If the criterion's value is GOOD or VERY_GOOD. The method returns the supporting_proposals list sorted by order of importance based on the agent's preferences.
 
-The method support_proposal, is used when the agent receives an ASK_WHY message after having proposed a sport. It takes the sport as a parameter and returns the strongest supportive argument for it by calling the List_Supporting_Proposal method and returning the first element of the resulting list.
+The method support_proposal, is used when the agent receives an ASK_WHY message after having proposed a sport. It takes the sport as a parameter and put the strongest supportive argument unseen so far for it by calling the List_Supporting_Proposal method.
 
 #### Attack
-The method List_Attacking_Proposal, does the same but this time to attack a sport, not support it.
+The method List_Attacking_Proposal, does the same but this time to attack a sport, not support it. But we decided not to use it because a counter-argument depends on the argument of the other agent.
 
 ### Relations among arguments
-The method attack_argument, decides whether an argument made by another agent can be attacked or not. The method checks three conditions to determine if an argument can be attacked or not:
+The method attack_argument, decides whether an argument made by another agent can be attacked or not. The method checks multiple conditions to determine if an argument can be attacked or not:
 * If the criterion is not important for the current agent, based on their order of importance
 * If the value given to the sport is lower than the one of the other agent on the considered criterion
 * If the current agent prefers another sport and can defend it by an argument with a better value on the same criterion
+* If the three conditions are not filled, the agent check if he can propose another sport not seen so far.
 
 If any of the above conditions is met, the method returns True and a number (1, 2, or 3) that indicates the reason for the attack. Otherwise, it returns False and 0.
 
@@ -62,7 +59,7 @@ We make sure the model's running attribute is set to False to indicate that the 
 
 #### Launch
 We made sure the simulation prints out every message that is exchanged during the discussion.
-Run the pw_argumentation.py file, sit back and watch the agents discuss!
+Run the pw_sport.py file, sit back and watch the agents discuss! It will simulate 100 discussions between two agents chosen randomly in the set of agents.
 
 ## Results
 Our 2-agent argumentation-based dialogue system is fully functional. We tested it with 2 different item types:
@@ -70,7 +67,8 @@ Our 2-agent argumentation-based dialogue system is fully functional. We tested i
 * Sports (10) - Own
 
 ### Statistics about our simulations
-
+For the simulation based on sports, we've got this histogram of the sports chosen on each of the 100 simulations.
+![Histogram](histogram.png "Number of times a sport is chosen")
 
 ## Next steps
 We need to make it multi-agent where interaction is still run between each pair of agents. This involves implementing new strategies for managing the dialogue flow as well as deciding when the common agreement is reached.
